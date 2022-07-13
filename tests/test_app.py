0160d0cdbad4299fb00d3d9da070cd7b244b6a82
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2022-07-12 20:46:28 -0500

    changes

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,10 @@ def test_index():
     # response = app.test_client().get("/")
     # assert response.status_code == 200
     # assert response.data.decode('utf-8') == 'Testing, Flask!'
+
+def test_message_post():
+    pass
+
+def test_message_get():
+    response = app.test_client().get("/message/hello")
+    assert (response != None)
